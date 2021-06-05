356690566eaa6a218c4c59d878aadf776b5738e3
Palak Gupta <palakgupta2712@gmail.com>
2021-06-05 14:20:27 +0530

    App.js: AmplifyAuthenticator added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React from "react";
+import Amplify from "aws-amplify";
+import { AmplifyAuthenticator, AmplifySignUp } from "@aws-amplify/ui-react";
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+import awsconfig from "./aws-exports";
 import Navbar from "./componets/Navbar";
 
-function App() {
-  return (
+Amplify.configure(awsconfig);
+
+const App = () => {
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
     <React.Fragment>
       <Navbar />
       <h1>Image Box</h1>
     </React.Fragment>
+  ) : (
+    <AmplifyAuthenticator>
+      <AmplifySignUp
+        slot="sign-up"
+        formFields={[
+          { type: "username" },
+          { type: "password" },
+          { type: "email" },
+        ]}
+      />
+    </AmplifyAuthenticator>
   );
-}
+};
 
 export default App;
